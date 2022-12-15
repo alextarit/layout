373dfd17802ce22cf449d4d6ea3bd7696e8a5176
tarit666 <79465997+alextarit@users.noreply.github.com>
2022-12-16 01:45:31 +0300

    did part layot supreme wrapper

---
+++ Supreme/js/dist/news.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Инициализируем Swiper
+new Swiper('.image-slider', {
+  //Стрелки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
